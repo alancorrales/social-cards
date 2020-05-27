8463341571d2d26894d64b448e34e0c699e23d9c
Alan <a.corrales@sidi.org.br>
2020-05-27 00:03:26 -0300

    Adds display of N cards (working with 10 images)

--- src/App.js
+++ src/App.js
@@ -1,31 +1,27 @@
 import React from 'react';
 import './App.css';
 import Posts from './posts/posts';
-import faker from 'faker';
-import { joinNames } from './utils/utils';
+import { generatePost } from './utils/utils'
+
 
 class App extends React.Component {
 
   state = {
-    posts: [
-      {
-        profileName: faker.company.companyName(),
-        profileAccount: joinNames(`@${faker.company.companyName()}`),
-        profileAvatar: faker.image.avatar(),
-        authorAccount: joinNames(`@${faker.name.findName()}`),
-        date: faker.date.recent().toDateString().split(new Date().getFullYear().toString())[0],
-        text: faker.lorem.sentences(),
-        card: {
-          image: `${process.env.PUBLIC_URL}/images/image${1}.jpg`,
-          title: faker.company.catchPhraseDescriptor(),
-          description: faker.company.catchPhrase(),
-          url: faker.internet.url()
-        },
-        comments: faker.random.number(100),
-        shares: faker.random.number(50),
-        likes: faker.random.number(300)
+    posts: []
+  }
+
+  componentDidMount() {
+    this.setState(state => {
+      let posts = [];
+
+      for (let i = 0; i < 10; i++) {
+        posts.push(generatePost(i + 1));
       }
-    ]
+
+      return {
+        posts
+      };
+    })
   }
 
   render() {
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,4 +1,26 @@
-export function joinNames(str) {
+import faker from 'faker';
+
+export function generatePost(index) {
+    return {
+        profileName: faker.company.companyName(),
+        profileAccount: joinNames(`@${faker.company.companyName()}`),
+        profileAvatar: faker.image.avatar(),
+        authorAccount: joinNames(`@${faker.name.findName()}`),
+        date: faker.date.recent().toDateString().split(new Date().getFullYear().toString())[0],
+        text: faker.lorem.sentences(),
+        card: {
+            image: `${process.env.PUBLIC_URL}/images/image${index}.jpg`,
+            title: faker.company.catchPhraseDescriptor(),
+            description: faker.company.catchPhrase(),
+            url: faker.internet.url()
+        },
+        comments: faker.random.number(100),
+        shares: faker.random.number(50),
+        likes: faker.random.number(300)
+    }
+}
+
+function joinNames(str) {
     const escapeChars = [
         ',', ';', '-', ' '
     ]
